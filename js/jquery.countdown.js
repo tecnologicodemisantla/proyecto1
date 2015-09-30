2cc185c42806676ff1723f629a998ebb42c8f85d
Irahan Jose Guzman <Irahan@Irahans-MacBook-Pro.local>
2015-09-30 00:28:12 -0500

    initial commit

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -111,7 +111,7 @@ and GPL-3.0 (http://opensource.org/licenses/GPL-3.0) licenses.
       return this.init();
     };
     $.countdown.defaultOptions = {
-      date: "June 7, 2087 15:03:25",
+      date: "November 20, 2015 10:00:00",
       refresh: 1000,
       onEnd: $.noop,
       render: function(date) {
