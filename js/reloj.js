56a02c616200f53e192fe533d34c5ad3c023fce6
Irahan Jose Guzman <Irahan@Irahans-MacBook-Pro.local>
2015-09-30 09:11:54 -0500

    initial commit

--- js/reloj.js
+++
@@ -1,8 +0,0 @@
-$('#clock').countdown('2015/11/20', function(event) {
-2   var $this = $(this).html(event.strftime(''
-3     + '<span>%w</span> weeks '
-4     + '<span>%d</span> days '
-5     + '<span>%H</span> hr '
-6     + '<span>%M</span> min '
-7     + '<span>%S</span> sec'));
-8 });
